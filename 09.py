f4d9f911f021e21e5051c210a1e5ce526b6ef644
ivankrtulgu <218090641+ivankrtulgu@users.noreply.github.com>
2025-12-06 17:08:06 +0300

    Сделана задача 9. Шаблон Jinja2 с данными.

---
+++ 09.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    users = ["Иван", "Мария", "Пётр"]
+    return render_template("index.html", users=users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
